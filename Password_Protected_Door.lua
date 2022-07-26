0fef290282576e18ed18c7a0b3d3a3d41e058253
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2022-07-26 03:20:29 -0700

    Update Password_Protected_Door.lua

--- Password_Protected_Door.lua
+++ Password_Protected_Door.lua
@@ -1,19 +1,28 @@
-local Timer = 5 -- Closes the door after this timer has passed
+local Timer = 2.5 -- Closes the door after this timer has passed
 local OutputSide = "Left" -- The side of the Computer the door is on
 local PassCode = "FissileFuel" -- The password
+
+function Type(Text)
+    for i=1,#Text do
+        write(string.sub(Text, i, i))
+        os.sleep()
+    end
+end
 
 -- Do not modify anything below this line
 while true do
     while true do
         term.clear()
-        term.setCursorPos(0,0)
-        write("Enter Passcode\n")
+        term.setCursorPos(1,1)
+        write("Enter passcode\n")
         write("> ")
         local Input = read("*")
 
-        if Input == PassCode then break end
+        if Input == PassCode then
+            Type("Correct passcode.")
+            break end
         print("Wrong passcode.")
-        os.sleep(2)
+        os.sleep(0.5)
     end
     rs.setOutput(OutputSide, true)
     os.sleep(Timer)
