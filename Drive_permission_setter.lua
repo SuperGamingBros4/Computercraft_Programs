12daf07ff0f8a07f4cb51410dfade33ab1694528
SuperGamingBros4 <60682866+SuperGamingBros4@users.noreply.github.com>
2022-07-29 23:14:28 -0700

    Update Drive_permission_setter.lua

--- Drive_permission_setter.lua
+++ Drive_permission_setter.lua
@@ -3,11 +3,14 @@ local Ids = {[7] = "4873-867"}
 local Drive = peripheral.find("drive")
 local Args = {...}
 
-local Level = Args[2]
+local Level = tonumber(Args[2])
 
 if not Drive then print("No Drive"); return; end
 if not Drive.isDiskPresent() then print("No Disk"); return; end
 
-if Level and type(Args[2]) == "number" then
-    Drive.setDiskLabel("Access level: " .. tostring(Ids[Level]) .. ", keycard")
+if Level and type(Level) == "number" then
+    Drive.setDiskLabel("Keycard, level: " .. tostring(Level))
+    local Keycard = fs.open("disk/id.txt", "w")
+    Keycard.write(Ids[Level])
+    Keycard.close()
 end
